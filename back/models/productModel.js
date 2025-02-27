1bf03b79aaf992dcc2c263920f5187e343a33252
Rnveer007 <rvs523474@gmail.com>
2025-02-27 14:45:25 +0530

    updates

--- back/models/productModel.js
+++ back/models/productModel.js
@@ -1,12 +1,28 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    brand: { type: String, required: true },
-    category: { type: String, required: true },
-    usualPrice: { type: Number, required: true },
-    discountPrice: { type: Number },
-    image: {type: String}
+    title: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    usualPrice: {
+        type: Number,
+        required: true
+    },
+    discountPrice: {
+        type: Number
+    },
+    image: {
+        type: String
+    }
 })
 
 const productData = mongoose.model("product", productSchema);
